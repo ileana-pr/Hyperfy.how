bb5c3a177f607aa12f618971ab4c3a650c0c559f
innkeeping <innkeeping@metagame.wtf>
2025-03-25 18:37:29 -0500

    rigid boddy

--- astro.config.mjs
+++ astro.config.mjs
@@ -130,7 +130,7 @@ export default defineConfig({
 								{ label: 'Num', slug: 'ref/num' },
 								{ label: 'Player', slug: 'ref/player' },
 								{ label: 'Props', slug: 'ref/props' },
-								{ label: 'rigidBody', slug: 'ref/rigidBody' },
+								{ label: 'rigidbody', slug: 'ref/rigid-body' },
 								{ label: 'UI', slug: 'ref/ui' },
 								{ label: 'UIText', slug: 'ref/ui-text' },
 								{ label: 'UIView', slug: 'ref/ui-view' },
